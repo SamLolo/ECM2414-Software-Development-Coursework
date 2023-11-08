24e69d213f85ad84c6277eaed0960cecbd9f3c8e
LAPTOP-31KT6NTB\Samue <sam.e.townley@icloud.com>
2023-11-08 21:06:16 +0000

    Error checking for player input

--- src/CardGame.java
+++ src/CardGame.java
@@ -7,12 +7,25 @@ public class CardGame {
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter the number of players:");
         int n = -1;
-        try {
-            n = input.nextInt();
-        } catch (InputMismatchException e) {
-            System.out.println("Number of players must be an integer!");
+        while (n <= 0) {
+            try {
+                n = input.nextInt();
+                if (n <= 0) {
+                    System.out.println("Number of players must be greater than 0!");
+                };
+            } catch (InputMismatchException e) {
+                System.out.println("Number of players must be an integer!");
+                n = -1;
+            } finally {
+                input.nextLine();
+            };
         };
-        System.out.println("You entered "+n);
+        System.out.println("You entered "+n+"\n");
+
+        System.out.println("Please enter the filename containing the pack of cards:");
+        String file = input.nextLine();
+        System.out.println("You entered "+file);
+        
         input.close();
     }
 }
