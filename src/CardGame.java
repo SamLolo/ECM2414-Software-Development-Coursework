9aa0bc4ece76ccb938de54d5673fd9a99eb86be1
LAPTOP-31KT6NTB\Samue <sam.e.townley@icloud.com>
2023-11-08 20:44:22 +0000

    Started main function

--- src/CardGame.java
+++ src/CardGame.java
@@ -1,3 +1,18 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class CardGame {
     
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the number of players:");
+        int n = -1;
+        try {
+            n = input.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Number of players must be an integer!");
+        };
+        System.out.println("You entered "+n);
+        input.close();
+    }
 }
