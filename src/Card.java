a023ef366f6838e9ade6c7b1b0c02505196d09f7
LAPTOP-31KT6NTB\Samue <sam.e.townley@icloud.com>
2023-11-09 11:50:59 +0000

    Added functionality to read and load pack file

--- src/Card.java
+++ src/Card.java
@@ -1,3 +1,12 @@
+
 public class Card {
-    
+    private int value = -1;
+
+    public Card(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
--- src/CardGame.java
+++ src/CardGame.java
@@ -1,7 +1,50 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 
 public class CardGame {
+
+    private static ArrayList<Card> loadPack(String file) {
+        ArrayList<Card> cards = new ArrayList<Card>();
+
+        try {
+            BufferedReader reader = new BufferedReader( new FileReader(file));
+            String value = reader.readLine();
+
+            while (value != null) {
+                int nValue = 0;
+                
+                try {
+                    nValue = Integer.parseInt(value);
+                    if (nValue <= 0) {
+                        throw new NumberFormatException("Negative Integer found!");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid value '"+value+"' found in "+file+"!");
+                    reader.close();
+                    return new ArrayList<Card>();
+                }
+                
+                cards.add(new Card(nValue));
+                value = reader.readLine();
+            }
+            
+            reader.close();
+            return cards;
+        
+        } catch (FileNotFoundException e) {
+            System.out.println("Cannot find file: "+file);
+            return cards;
+        
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return new ArrayList<Card>();
+        } 
+    }
     
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -22,10 +65,14 @@ public class CardGame {
         };
         System.out.println("You entered "+n+"\n");
 
+        ArrayList<Card> cards = new ArrayList<Card>();
         System.out.println("Please enter the filename containing the pack of cards:");
-        String file = input.nextLine();
+        while (cards.isEmpty()) {
+            String file = input.nextLine();
+            cards = loadPack(file);
+
         System.out.println("You entered "+file);
-        
         input.close();
+        }
     }
 }
