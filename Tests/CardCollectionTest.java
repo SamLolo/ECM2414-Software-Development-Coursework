d588307fdc1e0f4d025bc08b656ca40977843ba1
Charles_Symonds <charles.symonds.10@gmail.com>
2023-11-19 11:22:28 +0000

    Completed Tests For CardCollection. Created Nested classes containing tests which act as Test Suites. Created some minor Getter and setter functions to aid testing.

--- Tests/CardCollectionTest.java
+++ Tests/CardCollectionTest.java
@@ -1,33 +1,72 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CardCollectionTest {
+    CardCollection cardCollection = new CardCollection() {
+        @Override
+        public String toString() {
+            return null;
+        }
+    };
 
-    @Test
-    void addCard() {
+    private Card card1;
+    private Card card2;
+    private Card card3;
+    private Card card4;
+
+    ArrayList<Card> cards = new ArrayList<Card>();
+
+    CardCollectionTest() {
+    }
+
+    @BeforeEach
+    void setUp() {
+        card1 = new Card(3);
+        card2 = new Card(4);
+        card3 = new Card(6);
+        card4 = new Card(7);
+
+        cardCollection.addCard(card1);
+        cardCollection.addCard(card2);
+        cardCollection.addCard(card3);
+        cardCollection.addCard(card4);
+
+        cards.add(card1);
+        cards.add(card2);
+        cards.add(card3);
+        cards.add(card4);
     }
 
     @Test
-    void getCard() {
+    void testAddCards() {
+
+        assertEquals(4, cardCollection.size());
+        assertEquals(card1, cardCollection.getCard(0));
+        assertEquals(card2, cardCollection.getCard(1));
+
     }
 
     @Test
-    void getCards() {
-    }
-
-    @Test
-    void removeCard() {
+    void testGetAllCards() {
+        assertEquals(cards, cardCollection.getCards());
     }
 
     @Test
     void testRemoveCard() {
+        cardCollection.removeCard(0);
+        cards.remove(0);
+
+        assertEquals(cards, cardCollection.getCards());
+
     }
 
     @Test
-    void size() {
+    void testCardCollectionSize() {
+        assertEquals(cards.size(), cardCollection.size());
     }
 
-    @Test
-    void testToString() {
-    }
+
 }
