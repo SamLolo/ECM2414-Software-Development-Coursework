f6954fa65b5da3d3b5bca6afa55983de04dc0b25
LAPTOP-31KT6NTB\Samue <sam.e.townley@icloud.com>
2023-11-27 09:26:10 +0000

    Deck and Hand comments

--- src/Deck.java
+++ src/Deck.java
@@ -9,17 +9,25 @@ public class Deck extends CardCollection {
     private final int identifier;
     
     public Deck() {
+        // Instanciate extended CardCollection class and create unique integer identifer from static counter
         super();
         identifier = counter.incrementAndGet();
     }
 
     public void outputDeck() {
-        try {
-            BufferedWriter output = new BufferedWriter( new FileWriter("deck"+identifier+"_output.txt"));
-            output.write("deck "+identifier+" contents: "+toString());
-            output.close();
-        } catch(IOException ex) {
-            System.out.println(ex.getMessage());
+        // Create buffered file writer output for each deck and write the contents of the cards queue
+        int attempts = 3;
+        while (attempts >= 1) {
+            try {
+                BufferedWriter output = new BufferedWriter( new FileWriter("deck"+identifier+"_output.txt"));
+                output.write("deck "+identifier+" contents: "+toString());
+                output.close();
+                break;
+
+            // If IOException occurs, reduce max attempts by 1 and try again. If out of attempts, method will finish without output file
+            } catch(IOException ex) {
+                attempts -= 1;
+            }
         }
     }
 
--- src/Hand.java
+++ src/Hand.java
@@ -5,17 +5,23 @@ public class Hand extends CardCollection {
     private Random selector;
     
     public Hand() {
+        // Instancaiate extended CardCollection class and setup global random number generator
         super();
         selector = new Random();
     }
 
     public Card getRandomCard() {
+        // Convert protected BlockingQueue into array and get a random integer between 0 and 4 as there are 5 cards in the hand
         Object[] cardsArray = cards.toArray();
         int n = selector.nextInt(4);
+
+        // Return the Card stored at the value generated above in the Array, type casting to Card from Object
+        // This casting is safe as cardsArray will only contain Card objects
         return (Card)cardsArray[n];
     }
 
     public void removeCard(Card card) {
+        // If Card object passed in is inside Cards queue, remove the instance of that card
         if (cards.contains(card)) {
             cards.remove(card);
         }
